972b9ed1876b37ce5f175847c6e920c947dc8c81
bori00 <fazakasbori@gmail.com>
2021-12-27 22:00:59 +0200

    setup SOlarSystem class

---
+++ SolarSystem.cpp
@@ -0,0 +1,15 @@
+#include "SolarSystem.hpp"
+
+namespace view {
+	SolarSystem::SolarSystem() {
+		initPlanets();
+	}
+
+	void SolarSystem::render(const glm::mat4* base_model, const glm::mat4* view, long long seconds, GLint model_loc, GLint normal_matrix_loc) {
+
+	}
+
+	void SolarSystem::initPlanets() {
+
+	}
+}
