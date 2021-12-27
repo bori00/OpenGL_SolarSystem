da1deda16f73fb166c32cafe5042333a713f0903
bori00 <fazakasbori@gmail.com>
2021-12-27 22:00:36 +0200

    define PlanetView class

---
+++ PlanetView.cpp
@@ -0,0 +1,25 @@
+#include "PlanetView.hpp"
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+namespace view {
+	PlanetView::PlanetView(model::Planet planet, std::string object_filename, gps::Shader* shader) 
+			: planet_(planet), 
+			  shader_(shader) {
+		this->object_.LoadModel(object_filename);
+	}
+
+	void PlanetView::render(const glm::mat4* base_model, const glm::mat4* view, long long current_seconds, GLint model_loc, GLint normal_matrix_loc) {
+		glm::mat4 model = planet_.getPlanetSpecificModel(current_seconds) * (*base_model);
+
+		shader_->useShaderProgram();
+
+		glUniformMatrix4fv(model_loc, 1, GL_FALSE, glm::value_ptr(model));
+
+		glm::mat3 normalMatrix = glm::mat3(glm::inverseTranspose(*view * model));
+
+		glUniformMatrix3fv(normal_matrix_loc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+		object_.Draw(*shader_);
+	}
+}
---
+++ PlanetView.hpp
@@ -0,0 +1,30 @@
+#ifndef PlanetView_hpp
+#define PlanetView_hpp
+
+#include <memory>
+#include <string>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include "Planet.hpp"
+#include "TimedDrawable.h"
+#include "Shader.hpp"
+#include "Model3D.hpp"
+
+namespace view {
+	class PlanetView : public TimedDrawable
+	{
+	public:
+		PlanetView(model::Planet planet, std::string object_filename, gps::Shader* shader);
+
+		void render(const glm::mat4* base_model, const glm::mat4* view, long long seconds, GLint model_loc, GLint normal_matrix_loc);
+
+	private:
+		model::Planet planet_;
+		gps::Shader* shader_;
+		gps::Model3D object_;
+	};
+}
+
+#endif /* PlanetView_hpp */
+
+
