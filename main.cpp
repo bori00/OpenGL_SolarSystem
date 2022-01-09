790edc1bfb8fdd76f2958f4cffc6f05e6e00b425
bori00 <fazakasbori@gmail.com>
2022-01-10 01:56:22 +0200

    disable cursor

--- main.cpp
+++ main.cpp
@@ -389,7 +389,7 @@ void initOpenGLState() {
 	glEnable(GL_CULL_FACE); // cull face
 	glCullFace(GL_BACK); // cull back face
 	glFrontFace(GL_CCW); // GL_CCW for counter clock-wise
-    // glfwSetInputMode(myWindow.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(myWindow.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 }
 
 GLuint ReadTextureFromFile(const char* file_name) {
